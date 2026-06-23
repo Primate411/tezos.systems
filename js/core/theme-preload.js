d4288fde59aab29406f7c592a0a30f6133980b03
primate411 <primate@tez.capital>
2026-06-23 16:13:43 -0400

    Tighten uptime badge spacing

--- js/core/theme-preload.js
+++ js/core/theme-preload.js
@@ -8,7 +8,7 @@
  */
 (function () {
     var DEFAULT = 'aurora';
-    var THEME_CSS_VERSION = '278';
+    var THEME_CSS_VERSION = '279';
     var VALID = ['aurora', 'matrix', 'default', 'void', 'ember', 'signal', 'nerv', 'clean', 'dark', 'bubblegum', 'abyss', 'moss', 'warzone'];
     var t = null;
     try {
